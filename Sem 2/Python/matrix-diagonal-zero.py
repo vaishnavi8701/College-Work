513682c1c6e612858737f73a9b3d8391c94fdcf0
M.S.Madhan Kumar <msmadhankumar123@gmail.com>
2019-04-05 20:30:23 +0530

    Zero value to diagonal elements

--- Sem 2/Python/matrix-diagonal-zero.py
+++ Sem 2/Python/matrix-diagonal-zero.py
@@ -1,6 +1,6 @@
 """Assigning Zero value to all diagonal elements in a matrix"""
 
-matrix_A = [[1,2,3],[4,5,6],[7,8,9]]
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
 print("Displaying the matrix:\n")
 for i in range(3):
     for j in range(3):
@@ -10,7 +10,7 @@ print("Changing all diagonal elements to zero.")
 for i in range(3):
     for j in range(3):
         if (i == j):
-            matrix_A[i][j] = 0
+            matrix[i][j] = 0
 print("Displaying the Result matrix:\n")
 for i in range(3):
     for j in range(3):
